3e6489b13279e7118f208ae8883fae404734d249
dhirainarora <dhirainarora@gmail.com>
2025-09-23 15:29:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";   // 👈 must exactly match file name (App.jsx)
 import "./index.css";
 
-// Mount the React app into #root (from index.html)
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
+// Mount React app
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-
