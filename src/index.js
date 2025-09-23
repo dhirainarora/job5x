d502a1eccacb96215a2e80f87e3e25d19892a0b1
dhirainarora <dhirainarora@gmail.com>
2025-09-23 19:23:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";   // 👈 Try without `.jsx` (Vite resolves .jsx automatically)
-import "./index.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
