cea14d2eb37c23e10525f83acaf61dc4c818f0df
dhirainarora <dhirainarora@gmail.com>
2025-09-23 14:55:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";   // 👈 ensure App.jsx exists with this exact name
 import "./index.css";
 
+// Render React app into root div
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
