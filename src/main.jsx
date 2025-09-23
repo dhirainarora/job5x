8d69e5cb7866b94ab4278137f18721fdd7fa4b64
dhirainarora <dhirainarora@gmail.com>
2025-09-23 13:32:20 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
-// If you later add firebase client init, import it here: import "./firebase";
-
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
