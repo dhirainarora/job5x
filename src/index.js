c5b81510e0d5bae8edff1035ef12527db0bda3c8
dhirainarora <dhirainarora@gmail.com>
2025-09-23 19:28:04 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";   // 👈 force exact file name with .jsx extension
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
